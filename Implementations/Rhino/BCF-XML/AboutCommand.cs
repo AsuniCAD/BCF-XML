d45b6eb18c3d6837d7dbe3375f0f07236bff0dbf
kike-garbo <kike@asuni.com>
2018-11-15 17:06:56 +0100

    Added an empty RhinoCommon PlugIn project.

---
+++ Implementations/Rhino/BCF-XML/AboutCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+
+namespace BCFXML
+{
+	public class AboutCommand : Command
+	{
+		///<returns>The command name as it appears on the Rhino command line.</returns>
+		public override string EnglishName => "bcfAbout";
+
+		public AboutCommand()
+		{
+			// Rhino only creates one instance of each command class defined in a
+			// plug-in, so it is safe to store a refence in a static property.
+			Instance = this;
+		}
+
+		///<summary>The only instance of this command.</summary>
+		public static AboutCommand Instance
+		{
+			get; private set;
+		}
+
+		protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+		{
+			using (System.Diagnostics.Process.Start("https://github.com/AsuniCAD/BCF-XML")) { }
+
+			return Result.Success;
+		}
+	}
+}
---
+++ Implementations/Rhino/BCF-XML/Properties/AssemblyInfo.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+// Plug-in Description Attributes - all of these are optional.
+// These will show in Rhino's option dialog, in the tab Plug-ins.
+[assembly: PlugInDescription(DescriptionType.Address, "-")]
+[assembly: PlugInDescription(DescriptionType.Country, "-")]
+[assembly: PlugInDescription(DescriptionType.Email, "-")]
+[assembly: PlugInDescription(DescriptionType.Phone, "-")]
+[assembly: PlugInDescription(DescriptionType.Fax, "-")]
+[assembly: PlugInDescription(DescriptionType.Organization, "-")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "-")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "-")]
+
+// Icons should be Windows .ico files and contain 32-bit images in the following sizes: 16, 24, 32, 48, and 256.
+// This is a Rhino 6-only description.
+[assembly: PlugInDescription(DescriptionType.Icon, "BCF_XML.EmbeddedResources.BCFicon.ico")]
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BCF-XML")]
+
+// This will be used also for the plug-in description.
+[assembly: AssemblyDescription("BCF-XML utility plug-in")]
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BCF-XML")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cce2d3dc-75d9-40c1-b0b1-519d090a894f")] // This will also be the Guid of the Rhino plug-in
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+// Make compatible with Rhino Installer Engine
+[assembly: AssemblyInformationalVersion("2")]
