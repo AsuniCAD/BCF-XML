68bc83504cd112279346b3fdb9da994d01a49c63
kike-garbo <kike@asuni.com>
2018-11-16 20:30:23 +0100

    Parse doubles from XML using a cultural invariant IFormatProvider.

--- Implementations/Rhino/BCF-XML/Extensions.cs
+++ Implementations/Rhino/BCF-XML/Extensions.cs
@@ -1,4 +1,5 @@
 ﻿using System.Xml;
+using System.Globalization;
 
 namespace BCFXML
 {
@@ -15,9 +16,9 @@ namespace BCFXML
         /// <returns>Rhino vector</returns>
         public static Rhino.Geometry.Vector3d toVector(this XmlNode node, double scaleFactor = 1.0)
         {
-            double X = double.Parse(node.SelectSingleNode("X").InnerText);
-            double Y = double.Parse(node.SelectSingleNode("Y").InnerText);
-            double Z = double.Parse(node.SelectSingleNode("Z").InnerText);
+            double X = double.Parse(node.SelectSingleNode("X").InnerText, CultureInfo.InvariantCulture);
+            double Y = double.Parse(node.SelectSingleNode("Y").InnerText, CultureInfo.InvariantCulture);
+            double Z = double.Parse(node.SelectSingleNode("Z").InnerText, CultureInfo.InvariantCulture);
             return new Rhino.Geometry.Vector3d(X * scaleFactor, Y * scaleFactor, Z * scaleFactor);
         }
 
@@ -29,9 +30,9 @@ namespace BCFXML
         /// <returns>Rhino point</returns>
         public static Rhino.Geometry.Point3d toPoint(this XmlNode node, double scaleFactor = 1.0)
         {
-            double X = double.Parse(node.SelectSingleNode("X").InnerText);
-            double Y = double.Parse(node.SelectSingleNode("Y").InnerText);
-            double Z = double.Parse(node.SelectSingleNode("Z").InnerText);
+            double X = double.Parse(node.SelectSingleNode("X").InnerText, CultureInfo.InvariantCulture);
+            double Y = double.Parse(node.SelectSingleNode("Y").InnerText, CultureInfo.InvariantCulture);
+            double Z = double.Parse(node.SelectSingleNode("Z").InnerText, CultureInfo.InvariantCulture);
             return new Rhino.Geometry.Point3d(X * scaleFactor, Y * scaleFactor, Z * scaleFactor);
         }
     }
