a8bf7cbc306f98075339e2af09acc794996e671a
moethu <maximilian.o.e.thumfart@gmail.com>
2018-11-16 08:40:39 +0100

    Added ImportCommand

---
+++ Implementations/Rhino/BCF-XML/ImportCommand.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using Rhino;
+using Rhino.Commands;
+using Rhino.Geometry;
+using Rhino.Input;
+using Rhino.Input.Custom;
+using System.Xml;
+
+namespace BCFXML
+{
+    public class RhinoBCFImportCommand : Command
+    {
+        public RhinoBCFImportCommand()
+        {
+            // Rhino only creates one instance of each command class defined in a
+            // plug-in, so it is safe to store a refence in a static property.
+            Instance = this;
+        }
+
+        ///<summary>The only instance of this command.</summary>
+        public static RhinoBCFImportCommand Instance
+        {
+            get; private set;
+        }
+
+        ///<returns>The command name as it appears on the Rhino command line.</returns>
+        public override string EnglishName
+        {
+            get { return "bcfImport"; }
+        }
+
+        /// <summary>
+        /// Get XML Camera from VisInfo bcfv xml file
+        /// </summary>
+        /// <param name="filepath">bcfv xml file</param>
+        /// <param name="scaleFactor"></param>
+        /// <returns>Camera details as XMLCamera</returns>
+        public static XMLCamera GetXMLCameraFromVisInfo(string filepath, double scaleFactor)
+        {
+            // https://github.com/buildingSMART/BCF-XML/tree/release_2_1/Schemas
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(filepath);
+                XmlNode visinfo = doc.SelectSingleNode("VisualizationInfo");
+                XmlNode camera = visinfo.SelectSingleNode("PerspectiveCamera");
+                return new XMLCamera(camera, scaleFactor);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+        {
+            // 1) select BCF-Zip file
+            var fd = new Rhino.UI.OpenFileDialog { Filter = "BCF Files (*.bcfzip;*.bcf)|*.bcfzip;*.bcf" };
+            if (!fd.ShowOpenDialog())
+                return Rhino.Commands.Result.Cancel;
+
+            // 2) Extract all bcfv files from bcf
+            List<string> bcfvs = ExtractZip.ExtractBCFVFiles(fd.FileName);
+            foreach (string bcfv in bcfvs)
+            {
+                // 3) translate camera data from xml to XMLCamera
+                var cam = GetXMLCameraFromVisInfo(bcfv, 1.0);
+                if (cam == null) return Rhino.Commands.Result.Cancel;
+
+                // 4) get active view
+                var view = doc.Views.ActiveView;
+                Rhino.Display.RhinoViewport vp = view.ActiveViewport;
+                // save the current viewport projection
+                vp.PushViewProjection();
+
+                // 5) apply all camera settings to Rhino camera
+                vp.CameraUp = cam.UpVector;
+                vp.SetCameraLocation(cam.Location, false);
+                vp.SetCameraDirection(cam.Direction, true);
+                vp.Name = System.IO.Path.GetFileNameWithoutExtension(bcfv);
+                doc.NamedViews.Add(vp.Name, vp.Id);
+                view.Redraw();
+            }
+
+            return Result.Success;
+        }
+    }
+}
